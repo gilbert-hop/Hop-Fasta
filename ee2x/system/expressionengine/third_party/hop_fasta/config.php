d1991aecfe17060c67245854da5123ce1752968b
Louis DEKEISTER <louis@hopstudios.com>
2015-12-22 16:52:09 -0800

    First commit; EE3 WIP

---
+++ ee2x/system/expressionengine/third_party/hop_fasta/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Config file for Hop Fasta
+ *
+ * @package			Hop Fasta
+ * @version			2.2.0
+ * @author			Travis Smith <http://hopstudios.com/software>
+ * @copyright 		Copyright (c) 2015 Hop Studios <http://www.hopstudios.com>
+ * @license 		Commercial
+ * @link			http://www.hopstudios.com/software/versions/hop_fasta
+ */
+
+$config['name'] 	= "Hop Fasta";
+$config["version"] 	= "2.2.0";
+
+$config['nsm_addon_updater']['versions_xml'] = 'http://www.hopstudios.com/software/versions/fasta';
